d9ce0c6f19e54d718a56b9bb8df3087a56579afa
bShinzato <bshinzato@outlook.com>
2023-09-01 11:26:00 -0700

    added image

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -3,13 +3,15 @@ import React from 'react'
 const TopSection = () => {
   return (
    <>
-   <div className='grid gap-8 md:grid-cols-2 md:items-center md:text-center sm:items-center sm:text-center items-center text-center'>
+   <div className='grid gap-8 md:grid-cols-2 md:items-center md:text-center sm:items-center sm:text-center items-center text-center bg-gradient-to-r from-white to-sky-950'>
     <div>
    <h1 className='text-4xl'>Be Your Best Self</h1>
    <p className='text-lg'>I offer an exciting range of hairstyles combining the newest trends with care! </p>
 
     </div>
-   <img className="w-full md:w-2/3 md:h-2/3 rounded-full overflow-hidden shadow-2xl" src='../images/DarelieImg.jpg'/>
+
+   <img className="w-full md:w-2/3 md:h-2/3 rounded-full overflow-hidden" src='../images/Subject.png'/>
+    
    </div>
    </>
   )
