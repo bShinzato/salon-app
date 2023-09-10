10a455c565b140083dafeda4f5c82f9a623d476c
Brandon Shinzato <bshinzato@outlook.com>
2023-09-10 15:45:09 -0700

    added functionality for pictures for carousel.

--- src/CardData.js
+++ src/CardData.js
@@ -2,24 +2,38 @@ const cardData = [
   {
     title: "Haircuts",
     description: "Description for Card 1",
-    imageSrc: "https://pngimg.com/uploads/bride/bride_PNG19562.png",
+    imageSrc: [
+      "https://pngimg.com/uploads/bride/bride_PNG19562.png",
+      "https://i0.wp.com/www.hadviser.com/wp-content/uploads/2020/02/1-voluminous-lob-haircut-with-bangs-CLJg371BdpY.jpg?resize=985%2C1065&ssl=1",
+      "https://wavyhaircut.com/wp-content/uploads/2021/06/hairstyle-2021-asian-woman.jpg",
+    ],
   },
   {
     title: "Hair Color",
     description: "Description for Card 2",
-    imageSrc:
+    imageSrc: [
       "https://th.bing.com/th/id/R.a2c39b7e4725755e2aea5d071389b9cc?rik=lzWTmDGPNtLlag&riu=http%3a%2f%2f1.bp.blogspot.com%2f-WGjt6lxA1As%2fUZN3vR0Bt8I%2fAAAAAAAACzw%2f1Lwo1jAZWJc%2fs1600%2fbrunette%2bhighlights.jpg&ehk=bWbUkrpCKm%2fXacCeGxpHZ6xoyPpKaV2y%2bbSdhls5SM8%3d&risl=&pid=ImgRaw&r=0",
+      "https://th.bing.com/th/id/OIP.ihP7th9AScmPQ2EQScRPFQHaIl?pid=ImgDet&rs=1",
+      "https://grandbeautyspa.com/wp-content/uploads/2017/07/Hair-color.jpg",
+    ],
   },
   {
     title: "Facials",
     description: "Description for Card 2",
-    imageSrc: "https://c1.staticflickr.com/3/2888/9865582355_df71d00c02_b.jpg",
+    imageSrc: [
+      "https://c1.staticflickr.com/3/2888/9865582355_df71d00c02_b.jpg",
+      "https://th.bing.com/th/id/OIP.f_93qfJy6WKCnizJgWD80QHaJ4?w=138&h=184&c=7&r=0&o=5&pid=1.7",
+      "https://th.bing.com/th/id/R.282c606acc267252ffaf89fa7454872b?rik=wTefUQUzw86W9Q&riu=http%3a%2f%2fsanctumbeauty.co.uk%2fwp-content%2fuploads%2f2012%2f05%2fFacial.jpg&ehk=FoVwU85HqSC8k48kSt3YxfEjIW6VvZMGMHOUDpMIRlA%3d&risl=&pid=ImgRaw&r=0",
+    ],
   },
   {
     title: "Nails",
     description: "Description for Card 2",
-    imageSrc:
+    imageSrc: [
       "https://youhavestyle.com/wp-content/uploads/2020/11/125484065_280802486684550_7549447364354658045_n-864x1024.jpg",
+      "https://i.pinimg.com/originals/aa/11/f6/aa11f63caf1c64bfa805163338eabe99.jpg",
+      "https://i.pinimg.com/originals/89/54/ed/8954ed653cf633500c6c377edf65cdf7.jpg",
+    ],
   },
 ];
 
--- src/components/CardSlideshow.js
+++ src/components/CardSlideshow.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 import cardData from "../CardData";
+
+const CardImageSlideshow = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setOpacity(0);
+      setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setOpacity(1);
+      }, 1000);
+    }, 7000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [images]);
+
+  return (
+    <Image
+      alt="Card background"
+      src={images[currentImageIndex]}
+      style={{
+        transition: "opacity 1s ease-in-out",
+        opacity: opacity,
+      }}
+      className="object-cover rounded-xl w-48 h-48 md:h-64 md:w-64"
+    />
+  );
+};
 
 const CardSlideshow = () => {
   return (
     <>
       <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 p-2">
         {cardData.map((card, index) => (
-          <Card key={index} className="sm:w-60 hover:scale-105 shadow-lg">
+          <Card key={index} className="sm:w-60 shadow-lg">
             <CardHeader className="justify-center text-brownDark font-shadowsIntoLight">
               <h4 className="font-bold">{card.title}</h4>
             </CardHeader>
             <CardBody>
-              <Image
-                alt="Card background"
-                src={card.imageSrc}
-                className="object-cover rounded-xl w-48 h-48 md:h-64 md:w-64"
-              />
+              <CardImageSlideshow images={card.imageSrc} />
             </CardBody>
           </Card>
         ))}
